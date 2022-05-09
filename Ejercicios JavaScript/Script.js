d99ed9718c00fd57d112f86286a9d10e77bffd58
Angel0880 <elhardcase@outlook.com>
2022-05-09 16:21:22 -0300

    Clase 5 Terminada

---
+++ Ejercicios JavaScript/Script.js
@@ -0,0 +1,23 @@
+var id8 = document.getElementById("fuente-8")
+var id16 = document.getElementById("fuente-16")
+var id24 = document.getElementById("fuente-24")
+
+id8.addEventListener("click",function() {
+
+    document.body.style.fontSize = 8 + 'px';
+
+})
+
+id16.addEventListener("click",function() {
+
+    document.body.style.fontSize = 16 + 'px';
+
+
+})
+
+id24.addEventListener("click",function() {
+
+    document.body.style.fontSize = 24 + 'px';
+
+
+})
